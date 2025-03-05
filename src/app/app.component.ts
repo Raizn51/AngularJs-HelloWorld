ea75eb318ac70d2809483cfb374691f7fa96f56a
Raizn51 <Gouravraikwar989@gmail.com>
2025-03-05 21:34:09 +0530

    [Gourav Raikwar] Add . Ability to display Hello from BridgeLabz with a user inputted name in valid form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,7 @@ export class AppComponent {
   imgUrl= "../assets/BL_logo_square_jpg.jpg";
   url = "https://www.bridgelabz.com";
   userName: string = "";
+  nameError: string = "";
 
 
   ngOnInit(): void {
@@ -24,4 +25,15 @@ export class AppComponent {
     console.log("Save button is clicked!", $event);
     window.open(this.url, "_blank");
   }
+
+  onInput($event: Event) {
+    console.log("Change Event Occurred!", $event);
+    const nameRegex = /^[A-Z][a-zA-Z\s]{2,}$/; // Regex: First letter capital, min 3 characters
+    if (nameRegex.test(this.userName)) {
+      this.nameError = "";
+    } else {
+      this.nameError = "Name is Incorrect!";
+    }
+  }
+
 }
